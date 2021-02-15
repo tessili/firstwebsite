94b8624ed9d708e96126e1ff6c6c7a961e90d117
tessili <79107499+tessili@users.noreply.github.com>
2021-02-16 00:26:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ const hamburger = document.querySelector('.hamburger-btn');
 const navContainer = document.querySelector('.nav-menu');
 const navbar = document.querySelector('.navbar')
 const closeNavBtn = document.querySelector('.close-nav-btn');
-const directorsCardBtn = document.querySelectorAll('.directors-card-img__2');
-const directorsCardCloseBtn = document.querySelectorAll('.directors-back-card__close');
-const directorsFrontCard = document.querySelectorAll('.directors-front-card');
-const directorsBackCard = document.querySelectorAll('.directors-back-card');
 
 hamburger.addEventListener('click', toggleHamburger);
 closeNavBtn.addEventListener('click', closeNav);
@@ -29,21 +25,6 @@ navContainer.addEventListener('click', (e) => {
     }
 })
 
-directorsCardBtn.forEach((cardBtn, i) => {
-    cardBtn.addEventListener('click', () => {
-        directorsFrontCard[i].classList.toggle('hide-front-card');
-        directorsBackCard[i].style.display = 'block';
-        console.log ('clicked')
-    })
-})
-
-directorsCardCloseBtn.forEach((closeBtn, i) => {
-    closeBtn.addEventListener('click', () => {
-        directorsFrontCard[i].classList.remove('hide-front-card');
-        directorsBackCard[i].style.display = 'none';
-        console.log ('clicked')
-    })
-})
 
 
 
